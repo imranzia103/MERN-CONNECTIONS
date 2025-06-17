89463854447acc88e3092322f9051e6e01e6ad7f
Imran Zia <imranzia103@gmail.com>
2025-06-17 22:53:16 +0500

    Add files via upload
    new connection

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-import express from "express"
-import mongoose from "mongoose"
-import userRoutes from "./router/userRoutes.js";
-import dotenv from "dotenv"
+import express, { urlencoded } from "express";
+
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
-
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
-
-app.use("/users", userRoutes);
+app.use(urlencoded({ extended: true }));
 
 const connectDB = async () => {
-    try {await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected Sucessfully");
-        
-    } catch (error) {
-        console.error(`error:${error.message}`);
-        
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database connected Sucesfully...mongoDB");
+  } catch (error) {
+     console.error(`error:${error.message}`);
+  }
+};
 connectDB();
+
 app.listen(port, () => {
-    console.log(`server is running on port:${port}`);
+  console.log(`Server is Running on port: ${port}`);
 });
